e9d6012ab532765dc258f08322b834b52110b1b2
Belenov Dmitriy <dimabelenov@gmail.com>
2026-02-20 17:31:02 +0200

    feat: Add flutter_svg package and create AppImages for SVG asset paths.

---
+++ lib/core/theme/app_images.dart
@@ -0,0 +1,7 @@
+class AppImages {
+  AppImages._();
+
+  static const String logo = 'assets/images/logo.svg';
+  static const String placeholder = 'assets/images/placeholder.svg';
+  static const String arrow = 'assets/images/icon_arrow.svg';
+}
